83e9725525278c4292aa4cc07348de02f3b49bdf
asanjay123 <achintya_s@outlook.com>
2019-11-14 11:11:37 -0800

    Updated auto1
    Add movements without values to runop in auto 1
    Created hook var and implemented with correct variables in runop
    Added hook.map in init

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous_1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous_1.java
@@ -4,15 +4,18 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 @Autonomous(name = "Autonomous 1", group = "")
 public class Autonomous_1 extends LinearOpMode {
 
+    /**Define all values */
     DcMotor frontLeft;
     DcMotor backLeft;
     DcMotor frontRight;
     DcMotor backRight;
+    Servo hook;
     double  ElapsedTime;
     double COUNTS_PER_MOTOR_REV;
     double     DRIVE_GEAR_REDUCTION;
@@ -24,19 +27,38 @@ public class Autonomous_1 extends LinearOpMode {
     @Override
     public void runOpMode(){
 
+        /**Initialize autonomous
+         * Strafe to the corner
+         * Move backward
+         * Latch onto  platform
+         * Drive right (Forward straight, turn right)[not rotate]
+         * Drive back after platform is rotated 90 degrees
+         * Unlatch
+         * Drive forward and stop under bar
+         */
         initValues();
-        driveWithEncoder(.2, .1, .1, 30);
         driveWithStrafe(0, 0, 0, 0);
+        driveWithEncoder(.2, -0.1, -0.1, 30);
+        hook.setPosition(0.3);
+        driveWithEncoder(.2, 0.1, 0.1, 30);
+        driveWithEncoder(.2, -0.1, -0.1, 30);
+        hook.setPosition(0.65);
+        driveWithEncoder(0.2, .1, .1, 30);
+
 
 
     }
 
+    //Initialize values
     public void initValues(){
         frontLeft = hardwareMap.dcMotor.get("frontleft");
         backLeft = hardwareMap.dcMotor.get("backleft");
         frontRight = hardwareMap.dcMotor.get("frontright");
         backRight = hardwareMap.dcMotor.get("backright");
+        hook = hardwareMap.servo.get("hook");
 
+        //Defining front and back using clockwise and counterclockwise movement
+        // of the wheels
         backRight.setDirection(DcMotorSimple.Direction.REVERSE);
         frontRight.setDirection(DcMotorSimple.Direction.REVERSE);
 
@@ -45,16 +67,13 @@ public class Autonomous_1 extends LinearOpMode {
         double strafe = 1;
         double rotate = 0;
 
-
+        /**Movements for each motor*/
         double frontLeftPower = drive + strafe - rotate;
         double backLeftPower = drive - strafe - rotate;
         double frontRightPower = drive - strafe + rotate;
         double backRightPower = drive + strafe + rotate;
 
-
-
-
-
+        /**Revolutions for wheels given by gobuilda*/
         COUNTS_PER_MOTOR_REV    = 28;
         DRIVE_GEAR_REDUCTION    = 19.2 ;
         WHEEL_DIAMETER_INCHES   = 3.93701 ;
@@ -63,14 +82,10 @@ public class Autonomous_1 extends LinearOpMode {
     }
 
 
+    //Method to strafe
     public void driveWithStrafe(double speed, double strafeLeft, double strafeRight, double rotate) {
         int newLeftTarget;
         int newRightTarget;
-
-
-
-
-
 
 
         newLeftTarget = backLeft.getCurrentPosition() + (int)(strafeLeft * COUNTS_PER_INCH);
@@ -118,10 +133,6 @@ public class Autonomous_1 extends LinearOpMode {
                              double timeoutS) {
         int newLeftTarget;
         int newRightTarget;
-
-
-
-
 
 
 
