946bf13552af8a8150661b8245757450d83eb303
sashwatam <37467475+sashwatam@users.noreply.github.com>
2019-11-20 15:07:29 -0800

    stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TensorFlowLeft.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TensorFlowLeft.java
@@ -32,6 +32,11 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
 import java.util.List;
 import org.firstinspires.ftc.robotcore.external.ClassFactory;
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
@@ -49,13 +54,35 @@ import org.firstinspires.ftc.robotcore.external.tfod.Recognition;
  * IMPORTANT: In order to use this OpMode, you need to obtain your own Vuforia license key as
  * is explained below.
  */
-@Autonomous(name = "Concept: TensorFlow Object Detection Webcam", group = "Concept")
+@Autonomous(name = "Tensor Flow Left", group = "Concept")
 
-public class ConceptTensorFlowObjectDetectionWebcam extends LinearOpMode {
+public class TensorFlowLeft extends LinearOpMode {
     private static final String TFOD_MODEL_ASSET = "Skystone.tflite";
     private static final String LABEL_FIRST_ELEMENT = "Stone";
     private static final String LABEL_SECOND_ELEMENT = "Skystone";
+    int objectsSize;
+    double position;
 
+    boolean skystoneFound;
+    int skystonePos;
+    DcMotor frontLeft;
+    DcMotor backLeft;
+    DcMotor frontRight;
+    DcMotor backRight;
+    Servo hook;
+    double  ElapsedTime;
+    double COUNTS_PER_MOTOR_REV;
+    double     DRIVE_GEAR_REDUCTION;
+    double     WHEEL_DIAMETER_INCHES;
+
+    double     COUNTS_PER_INCH;
+    DcMotor armmotor;
+
+    Servo   servo;
+    Servo servo1;
+    Servo servo2;
+
+    com.qualcomm.robotcore.util.ElapsedTime runtime = new ElapsedTime();
     /*
      * IMPORTANT: You need to obtain your own license key to use Vuforia. The string below with which
      * 'parameters.vuforiaLicenseKey' is initialized is for illustration only, and will not function.
@@ -97,6 +124,7 @@ public class ConceptTensorFlowObjectDetectionWebcam extends LinearOpMode {
     public void runOpMode() {
         // The TFObjectDetector uses the camera frames from the VuforiaLocalizer, so we create that
         // first.
+        initValues();
         initVuforia();
 
         if (ClassFactory.getInstance().canCreateTFObjectDetector()) {
@@ -119,6 +147,13 @@ public class ConceptTensorFlowObjectDetectionWebcam extends LinearOpMode {
         waitForStart();
 
         if (opModeIsActive()) {
+
+            driveWithEncoder(0.2,4,4,30);
+
+            strafeWithTime(2,0.2,'r');
+
+            driveWithEncoder(0.2,44,44,30);
+
             while (opModeIsActive()) {
                 if (tfod != null) {
                     // getUpdatedRecognitions() will return null if no new information is available since
@@ -135,11 +170,39 @@ public class ConceptTensorFlowObjectDetectionWebcam extends LinearOpMode {
                                     recognition.getLeft(), recognition.getTop());
                             telemetry.addData(String.format("  right,bottom (%d)", i), "%.03f , %.03f",
                                     recognition.getRight(), recognition.getBottom());
+
+                            if(recognition.getLabel().equals("Skystone")) {
+                                skystoneFound = true;
+                                skystonePos = i;
+                            }
                         }
                         telemetry.update();
                     }
+
+                    if(skystoneFound) {
+
+                        break;
+
+                    }
+                    if(objectsSize < updatedRecognitions.size()) {
+
+                        objectsSize++;
+                        strafeWithTime(0.1,0.4,'l');
+
+                    }
+
                 }
             }
+            servo1.setPosition(0.7);
+
+            armmotor.setPower(-0.8);
+            sleep(600);
+
+            servo.setPosition(0.08);
+            servo1.setPosition(0.216);
+            armmotor.setPower(0.8);
+            sleep(600);
+
         }
 
         if (tfod != null) {
@@ -150,6 +213,212 @@ public class ConceptTensorFlowObjectDetectionWebcam extends LinearOpMode {
     /**
      * Initialize the Vuforia localization engine.
      */
+    public void initValues(){
+        armmotor = hardwareMap.dcMotor.get("arm");
+        servo = hardwareMap.servo.get("servo");
+        servo1 = hardwareMap.servo.get("servo1");
+        servo2 = hardwareMap.servo.get("servo2");
+        position = 0.8;
+        servo.setPosition(position);
+        servo1.setPosition(0.216);
+        servo2.setPosition(0);
+
+        frontLeft = hardwareMap.dcMotor.get("frontleft");
+        backLeft = hardwareMap.dcMotor.get("backleft");
+        frontRight = hardwareMap.dcMotor.get("frontright");
+        backRight = hardwareMap.dcMotor.get("backright");
+        hook = hardwareMap.servo.get("hook");
+
+        //Defining front and back using clockwise and counterclockwise movement
+        // of the wheels
+        backRight.setDirection(DcMotorSimple.Direction.REVERSE);
+        //Initialize values.REVERSE);
+        frontRight.setDirection(DcMotorSimple.Direction.REVERSE);
+
+
+        double drive = 0;
+        double strafe = 1;
+        double rotate = 0;
+
+        /**Movements for each motor*/
+        double frontLeftPower = drive + strafe - rotate;
+        double backLeftPower = drive - strafe - rotate;
+        double frontRightPower = drive - strafe + rotate;
+        double backRightPower = drive + strafe + rotate;
+
+        /**Revolutions for wheels given by gobuilda*/
+        COUNTS_PER_MOTOR_REV    = 28;
+        DRIVE_GEAR_REDUCTION    = 19.2 ;
+        WHEEL_DIAMETER_INCHES   = 3.93701 ;
+        COUNTS_PER_INCH         = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) / (WHEEL_DIAMETER_INCHES * 3.1415);
+
+    }
+    public void driveWithStrafe(double speed, double strafe, double rotate) {
+
+        int newLeftTarget;
+        int newRightTarget;
+
+
+        newLeftTarget = backLeft.getCurrentPosition() + (int)(strafe * COUNTS_PER_INCH);
+        newRightTarget = backRight.getCurrentPosition() + (int)(strafe * COUNTS_PER_INCH);
+
+
+        backLeft.setTargetPosition(newLeftTarget);
+        backRight.setTargetPosition(newRightTarget);
+        backLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        backRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        runtime.reset();
+
+        if (strafe > 0){
+            //Strafe right
+            backLeft.setPower(speed);
+            backRight.setPower(speed);
+            frontLeft.setPower(speed);
+            frontRight.setPower(speed);
+        }
+        else if (strafe < 0){
+            //Strafe left
+            backLeft.setPower(-speed);
+            backRight.setPower(speed);
+            frontLeft.setPower(speed);
+            frontRight.setPower(-speed);
+
+
+        }
+
+
+
+        while (
+                backLeft.getCurrentPosition() < newLeftTarget &&
+                        backRight.getCurrentPosition() < newRightTarget )
+        {
+
+            telemetry.addData("Path1",  "Running to %7d :%7d", newLeftTarget,  newRightTarget);
+            telemetry.addData("Path2",  "Running at %7d :%7d",
+                    backLeft.getCurrentPosition(),
+                    backRight.getCurrentPosition());
+            telemetry.update();
+
+        }
+
+        backLeft.setPower(0);
+        backRight.setPower(0);
+        frontLeft.setPower(0);
+        frontRight.setPower(0);
+
+        backLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        backRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        //  sleep(250);
+
+
+
+
+
+
+
+
+    }
+
+    public void strafeWithTime(double time, double power, char direction){
+        long initialTime = System.currentTimeMillis();
+        double finalTime = initialTime + time*1000;
+
+        if (direction == 'l'){
+            backLeft.setPower(power);
+            backRight.setPower(-power);
+            frontLeft.setPower(-power);
+            frontRight.setPower(power);
+        }
+
+        if (direction == 'r'){
+            backLeft.setPower(-power);
+            backRight.setPower(power);
+            frontLeft.setPower(power);
+            frontRight.setPower(-power);
+        }
+        if (direction == 'b'){
+            backLeft.setPower(-power);
+            backRight.setPower(-power);
+            frontLeft.setPower(-power);
+            frontRight.setPower(-power);
+        }
+        if (direction == 'p'){
+            backLeft.setPower(power);
+            backRight.setPower(-power);
+            frontLeft.setPower(power);
+            frontRight.setPower(-power);
+        }
+        if (direction == 'q'){
+            backLeft.setPower(-power);
+            backRight.setPower(power);
+            frontLeft.setPower(-power);
+            frontRight.setPower(power);
+        }
+        while (System.currentTimeMillis() < finalTime){
+
+        }
+        backLeft.setPower(0);
+        backRight.setPower(0);
+        frontLeft.setPower(0);
+        frontRight.setPower(0);
+    }
+
+    public void driveWithEncoder(double speed,
+                                 double leftInches, double rightInches,
+                                 double timeoutS) {
+        int newLeftTarget;
+        int newRightTarget;
+
+
+
+        newLeftTarget = backLeft.getCurrentPosition() + (int)(leftInches * COUNTS_PER_INCH);
+        newRightTarget = backRight.getCurrentPosition() + (int)(rightInches * COUNTS_PER_INCH);
+
+        if (speed > 0){
+
+            backLeft.setTargetPosition(newLeftTarget);
+            backRight.setTargetPosition(newRightTarget);
+        }else{
+            backLeft.setTargetPosition(-newLeftTarget);
+            backRight.setTargetPosition(-newRightTarget);
+
+        }
+        backLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        backRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        runtime.reset();
+
+        backLeft.setPower(speed);
+        backRight.setPower(speed);
+
+
+        frontLeft.setPower(speed);
+        frontRight.setPower(speed);
+
+
+        while (
+                backLeft.getCurrentPosition() < newLeftTarget &&
+                        backRight.getCurrentPosition() < newRightTarget )
+        {
+
+            telemetry.addData("Path1",  "Running to %7d :%7d", newLeftTarget,  newRightTarget);
+            telemetry.addData("Path2",  "Running at %7d :%7d",
+                    backLeft.getCurrentPosition(),
+                    backRight.getCurrentPosition());
+            telemetry.update();
+
+        }
+        backLeft.setPower(0);
+        backRight.setPower(0);
+        frontLeft.setPower(0);
+        frontRight.setPower(0);
+
+        backLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        backRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        //  sleep(250);
+
+    }
     private void initVuforia() {
         /*
          * Configure Vuforia by creating a Parameter object, and passing it to the Vuforia engine.
