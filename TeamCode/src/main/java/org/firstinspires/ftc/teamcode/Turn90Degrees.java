da39c6450c36644e874c65bb65b23ca0fc7dd272
NubCoder <36945035+NubCoder@users.noreply.github.com>
2020-01-09 22:46:47 -0800

    Update Turn90Degrees.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Turn90Degrees.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Turn90Degrees.java
@@ -11,8 +11,7 @@ import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
 import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
 import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
 
-@Autonomous(name="IMU STUFF", group="")
-//@Disabled
+@Autonomous(name="IMU Testing Stuff", group="")
 public class Turn90Degrees extends LinearOpMode
 {
     DcMotor                 frontLeft, frontRight, backLeft, backRight;
@@ -33,41 +32,24 @@ public class Turn90Degrees extends LinearOpMode
         telemetry.addData("Mode", "running");
         telemetry.update();
 
-        sleep(1000);
-
-        // drive until end of period.
-
-        while (opModeIsActive())
-        {
-            /**
-            // Use gyro to drive in a straight line.
-            correction = checkDirection();
 
 
 
-            double power = .3;
-            frontLeft.setPower(power - correction);
-            backLeft.setPower(power - correction);
-            frontRight.setPower(power + correction);
-            backRight.setPower(power + correction);
 
-            */
+        telemetry.addData("1 imu heading", lastAngles.firstAngle);
+        telemetry.addData("2 global heading", globalAngle);
+        telemetry.update();
 
-            telemetry.addData("1 imu heading", lastAngles.firstAngle);
-            telemetry.addData("2 global heading", globalAngle);
-            telemetry.update();
+        driveStraightWithCorrection(5, .2, 'f');
+        strafeWithCorrection(5, .2, 'r');
+        rotate(90, .15);
+        sleep(500);
 
 
-            rotate(90, .15);
-            sleep(500);
-
-        }
 
     }
 
-    /**
-     * Resets the cumulative angle tracking to zero.
-     */
+
     private void resetAngle()
     {
         lastAngles = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
@@ -75,17 +57,9 @@ public class Turn90Degrees extends LinearOpMode
         globalAngle = 0;
     }
 
-    /**
-     * Get current cumulative angle rotation from last reset.
-     * @return Angle in degrees. + = left, - = right.
-     */
+
     private double getAngle()
     {
-        // We experimentally determined the Z axis is the axis we want to use for heading angle.
-        // We have to process the angle because the imu works in euler angles so the Z axis is
-        // returned as 0 to +180 or 0 to -180 rolling back to -179 or +179 when rotation passes
-        // 180 degrees. We detect this transition and track the total cumulative angle of rotation.
-
         Orientation angles = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
 
         double deltaAngle = angles.firstAngle - lastAngles.firstAngle;
@@ -102,33 +76,24 @@ public class Turn90Degrees extends LinearOpMode
         return globalAngle;
     }
 
-    /**
-     * See if we are moving in a straight line and if not return a power correction value.
-     * @return Power adjustment, + is adjust left - is adjust right.
-     */
     private double checkDirection()
     {
-        // The gain value determines how sensitive the correction is to direction changes.
-        // You will have to experiment with your robot to get small smooth direction changes
-        // to stay on a straight line.
+
         double correction, angle, gain = .10;
 
         angle = getAngle();
 
         if (angle == 0)
-            correction = 0;             // no adjustment.
+            correction = 0;
         else
-            correction = -angle;        // reverse sign of angle for correction.
+            correction = -angle;
 
         correction = correction * gain;
 
         return correction;
     }
 
-    /**
-     * Rotate left or right the number of degrees. Does not support turning more than 180 degrees.
-     * @param degrees Degrees to turn, + is left - is right
-     */
+
     private void rotate(int degrees, double power)
     {
         double  leftPower, rightPower;
@@ -141,12 +106,12 @@ public class Turn90Degrees extends LinearOpMode
 
         if (degrees < 0)
         {   // turn right.
-            leftPower = power;
-            rightPower = -power;
+            leftPower = -power;
+            rightPower = power;
         }
         else if (degrees > 0)
         {   // turn left.
-            leftPower = -power;
+            leftPower = power;
             rightPower = power;
         }
         else return;
@@ -157,28 +122,85 @@ public class Turn90Degrees extends LinearOpMode
         frontRight.setPower(rightPower);
         backRight.setPower(rightPower);
 
-        // rotate until turn is completed.
         if (degrees < 0)
         {
-            // On right turn we have to get off zero first.
             while (opModeIsActive() && getAngle() == 0) {}
 
             while (opModeIsActive() && getAngle() > degrees) {}
         }
-        else    // left turn.
+        else
             while (opModeIsActive() && getAngle() < degrees) {}
 
-        // turn the motors off.
         frontLeft.setPower(0);
         backLeft.setPower(0);
         frontRight.setPower(0);
         frontLeft.setPower(0);
 
-        // wait for rotation to stop.
-        sleep(1000);
 
-        // reset angle tracking on new heading.
         resetAngle();
+    }
+
+    private void driveStraightWithCorrection(int time, double power, char direction){
+        long initialTime = System.currentTimeMillis();
+        double finalTime = initialTime + time*1000;
+
+        if (direction == 'f') {
+
+            while (System.currentTimeMillis() < finalTime){
+                correction = checkDirection();
+                frontLeft.setPower(power - correction);
+                backLeft.setPower(power - correction);
+                frontRight.setPower(power + correction);
+                backRight.setPower(power + correction);
+            }
+        }
+
+        if (direction == 'b') {
+            while (System.currentTimeMillis() < finalTime){
+                correction = checkDirection();
+                frontLeft.setPower(-power + correction);
+                backLeft.setPower(-power + correction);
+                frontRight.setPower(-power - correction);
+                backRight.setPower(-power - correction);
+            }
+        }
+
+
+        backLeft.setPower(0);
+        backRight.setPower(0);
+        frontLeft.setPower(0);
+        frontRight.setPower(0);
+    }
+
+    private void strafeWithCorrection(int time, double power, char direction){
+        long initialTime = System.currentTimeMillis();
+        double finalTime = initialTime + time*1000;
+
+        if (direction == 'f') {
+            while (System.currentTimeMillis() < finalTime){
+                correction = checkDirection();
+                frontLeft.setPower(power - correction);
+                backLeft.setPower(-power + correction);
+                frontRight.setPower(power + correction);
+                backRight.setPower(-power - correction);
+            }
+        }
+
+        if (direction == 'b') {
+            while (System.currentTimeMillis() < finalTime){
+                correction = checkDirection();
+                frontLeft.setPower(+power - correction);
+                backLeft.setPower(-power + correction);
+                frontRight.setPower(-power - correction);
+                backRight.setPower(power + correction);
+            }
+        }
+
+
+        backLeft.setPower(0);
+        backRight.setPower(0);
+        frontLeft.setPower(0);
+        frontRight.setPower(0);
     }
 
     public void initIMU(){
@@ -190,9 +212,6 @@ public class Turn90Degrees extends LinearOpMode
         parameters.accelUnit           = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
         parameters.loggingEnabled      = false;
 
-        // Retrieve and initialize the IMU. We expect the IMU to be attached to an I2C port
-        // on a Core Device Interface Module, configured to be a sensor of type "AdaFruit IMU",
-        // and named "imu".
         imu = hardwareMap.get(BNO055IMU.class, "imu");
 
         imu.initialize(parameters);
@@ -200,7 +219,6 @@ public class Turn90Degrees extends LinearOpMode
         telemetry.addData("Mode", "calibrating...");
         telemetry.update();
 
-        // make sure the imu gyro is calibrated before continuing.
         while (!isStopRequested() && !imu.isGyroCalibrated())
         {
             sleep(50);
